b692599663153afa3650bbc06ed229950dcca9b3
samanthaluo <243020924@qq.com>
2016-04-15 18:49:09 +0800

    增加本地include

--- index.js
+++ index.js
@@ -1,62 +1,91 @@
 'use stric'
-var gulp = require('gulp');
-var defaults = require('defaults');
+
+var path = require('path');
 var fs = require('fs');
-var async = require('async');
 var request = require('request');
 var progress = require('request-progress');
-var path = require('path');
 var parseurl = require('parseurl');
+var async = require('async');
 var iconv = require('iconv-lite');
+var defaults = require('defaults');
 
+var includeFileReg=/<!--#\s*include\s+(file|virtual)=(['"])([^\r\n]+?)\2(\s+domain=(['"])([^\r\n]+?)\5)?\s*-->/g;
+function resolveIncludes(content, opt, callback){
+	content = content.toString();
+		async.whilst(
+			function test() {
+				includeFileReg.lastIndex =0;
+				return !!(matches = includeFileReg.exec(content));
+			},
+			function insertInclude(innerNext){
 
-module.exports = function connectIncludeOnline(opt){
+				var shortPath = matches[3];
+				var domain = matches[6];
+				if(!domain){
+					var tpath =  path.join(opt.baseDir, shortPath);
+					fs.readFile(tpath, {}, function(err, innerContentRaw) {
+                        if (err) {
+                            return innerNext(err);
+                        }
+						content = content.slice(0,matches.index)+innerContentRaw+content.slice(matches.index + matches[0].length);
+                        innerNext(null, content);
+                      		
+                        }
+                    );
+				}else{
+				var src=domain+shortPath;
+				progress(request({url:src,encoding:null},function(err,innerRes,innerContent){
+					if(err){
+						return innerNext(err);	
+					}
+
+					if(opt.encoding.toLowerCase()=='gbk'){
+						innerContent= iconv.decode(innerContent, 'GBK');//return unicode string from GBK encoded bytes
+					}
+					var first = matches.index;
+					var second=matches.index + matches[0].length;
+					content = content.slice(0,first)+innerContent+content.slice(second);
+					innerNext(null, content);
+				}),{throttle:1000,delay:1000});
+				}
+			},
+			function includesComplete(err){
+				if(err){
+					return callback(err);
+				}
+				return callback(null, content);
+			}
+		);	
+}
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
+
+module.exports = function(opt){
 	opt = defaults(opt, {
     	baseDir: '.',
     	ext: '.html',
     	encoding:''
   	});
-  	function endsWith(str, suffix) {
-	    return str.indexOf(suffix, str.length - suffix.length) !== -1;
-	}
+
   	return function(req,res,next){
   		var url = parseurl(req).pathname;
 		if(!endsWith(url, opt.ext)) {
 	    	return next();
 	    }
+		var filePath = path.join(opt.baseDir, url);
+		//文件读取
 		fs.readFile(filePath,{},function(err,content){
 			if(err){ //读取当前文件出错，继续下一个文件
-				console.log('error'+count);
 				return next();
 			}
-			async.whilst(
-				function test() {return !!(matches = includeFileReg.exec(content)); },
-				function insertInclude(innerNext){
-					var domain = matches[2];
-					var shortPath = matches[5];
-					var src=domain+shortPath;
-					progress(request({url:src,encoding:null},function(err,innerRes,innerContent){
-						if(err){
-							return innerNext(err);	
-						}
-						if(opt.encoding.toLowerCase()=='gbk'){
-							innerContent= iconv.decode(innerContent, 'GBK');//return unicode string from GBK encoded bytes
-						}
-						var first = matches.index;
-						var second=matches.index + matches[0].length;
-						innerNext(null, content);
-					}),{throttle:1000,delay:1000});
-				},
-				function includesComplete(err){
-					if(err){
-						return next();
-					}
-					res.end(content);
+			resolveIncludes(content, opt, function(err, content){
+				if(err){
+					return next();
 				}
-			);
-			
+				res.end(content);
+			})
 		});
   	}
 }
-
-
